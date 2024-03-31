4d4c37552a92e3ef90cb5bdec61f1b1f653c8c48
rendyputrak <rendyputrak@gmail.com>
2024-03-31 10:46:27 +0700

    Edit beranda sedikit

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -7,15 +7,16 @@
 </head>
 
 <body>
-  <div class="container min-h-screen bg-gradient-to-br from-blue-500 to-cyan-500 px-10 py-5">
+  <div class="container min-h-screen bg-gradient-to-br from-blue-500 to-cyan-500 px-10 py-5 relative">
     @include('template.header')
 
     <div class="px-5 my-40">
-      <h1 class="text-white text-5xl font-bold leading-normal mb-32">RW 03 Bunulrejo,<br>Kec. Blimbing,<br>Kota Malang</h1>
+      <h1 class="text-white text-5xl font-bold leading-normal mb-28">RW 03 Bunulrejo,<br>Kec. Blimbing,<br>Kota Malang</h1>
       <span class="text-white text-3xl font-medium leading-normal mt-24">Pengumuman<br>dan Berita</span>
       <span class="text-yellow-400 text-3xl font-medium leading-normal mt-24">Terkini</span>
     </div>
 
+    <img src="../img/tes.png" class="w-full xl:w-1/2 xl:absolute bottom-0 right-20">
   </div>
 </body>
 </html>
--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -3,10 +3,10 @@
       <li class="list-none inline-block px-5"><a href="{{url('beranda')}}" class="underline text-white font-semibold px-2">RW 03 - Bunulrejo</a></li>
     </ul>
     <ul class="flex-1 text-right">
-      <li class="list-none inline-block px-5"><a href="{{url('beranda')}}" class="underline text-white px-2">Beranda</a></li>
-      <li class="list-none inline-block px-5"><a href="#" class="underline text-white px-2">Tentang</a></li>
-      <li class="list-none inline-block px-5"><a href="#" class="underline text-white px-2">UMKM</a></li>
-      <li class="list-none inline-block px-5"><a href="#" class="underline text-white px-2">Berita</a></li>
-      <li class="list-none inline-block px-5"><a href="#" class="underline text-white px-2">Kontak</a></li>
+      <li class="list-none inline-block px-5"><a href="{{url('beranda')}}" class="text-white px-2 hover:underline-offset-auto">Beranda</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class=" text-white px-2 hover:underline">Tentang</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-8">UMKM</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-auto">Berita</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-auto">Kontak</a></li>
     </ul>
 </nav>
