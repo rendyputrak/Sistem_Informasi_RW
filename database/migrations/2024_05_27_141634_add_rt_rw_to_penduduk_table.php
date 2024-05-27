023f02a4af8d0af286c7c4cb20da425abf2b2463
rendyputrak <rendyputrak@gmail.com>
2024-05-27 15:06:15 +0700

    edit migration

---
+++ database/migrations/2024_05_27_141634_add_rt_rw_to_penduduk_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('penduduk', function (Blueprint $table) {
+            $table->string('RT', 5)->nullable()->after('status_kependudukan');
+            $table->string('RW', 5)->nullable()->after('RT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('penduduk', function (Blueprint $table) {
+            $table->dropColumn('RT');
+            $table->dropColumn('RW');
+        });
+    }
+};
