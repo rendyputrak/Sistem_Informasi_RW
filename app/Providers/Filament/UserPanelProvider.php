a9126c50c318f760e8385c49831a9a150a4c00db
rendyputrak <rendyputrak@gmail.com>
2024-06-09 18:28:38 +0700

    custom login

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Auth\CustomLogin;
 use App\Http\Middleware\VerifyRole;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,7 +27,7 @@ class UserPanelProvider extends PanelProvider
         return $panel
             ->id('user')
             ->path('user')
-            ->login()
+            ->login(CustomLogin::class)
             // ->registration()
             ->profile()
             ->colors([
